28cf0d42912e48993fff29b85107ce115e2912c6
Huỳnh Lâm Quốc Việt <xa.lach@yahoo.com>
2015-05-20 01:34:23 +0700

    Update torviet-shoutbox-enhancer.user.js

--- torviet-shoutbox-enhancer.user.js
+++ torviet-shoutbox-enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TorViet Shoutbox Enhancer
 // @namespace    http://torviet.com/userdetails.php?id=1662
-// @version      0.5.3
+// @version      0.5.4
 // @license      http://www.wtfpl.net/txt/copying/
 // @homepageURL  https://github.com/S-a-l-a-d/TorViet-Shoutbox-Enhancer
 // @supportURL   https://github.com/S-a-l-a-d/TorViet-Shoutbox-Enhancer/issues
@@ -90,7 +90,9 @@
             case 38:
                 emoGroup.selectedIndex !== 0 && emoGroup.selectedIndex--,
                     changeEmoGroup();
+                break;
             default:
+                idQuestion.focus();
         }
     }
 
